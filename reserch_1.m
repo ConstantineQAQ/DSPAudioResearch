6e9ac9ad3426e845beeb989a41654def79f54765
ConstantineQAQ <357208746@qq.com>
2022-11-07 21:48:00 +0800

    first push

---
+++ reserch_1.m
@@ -0,0 +1,23 @@
+clc;
+clear;
+close all;
+
+filename='G:\MATLAB2021a\final\wyn\jiuwangleba.wav';
+[info,Fs]=audioread(filename);
+disp(Fs);
+
+N=length(info);
+T=1/Fs;
+t=(0:N-1)*T;
+
+df=Fs/N;
+f=(0:N/2)*df;
+
+info_fft=fft(info);
+info_fft=abs(info_fft/N);
+info_fft1=info_fft(1:N/2+1);
+info_fft1(2:end-1) = 2*info_fft1(2:end-1);
+
+
+subplot(2,1,1);plot(t,info);title('对歌曲44100hz采样');ylabel('时域离散信号');xlabel('n');
+subplot(2,1,2);plot(f,info_fft1,'blue');title('对歌曲44100hz采样的频域图');ylabel('频域域离散信号');xlabel('f');
---
+++ reserch_2.m
@@ -0,0 +1,34 @@
+clc;
+clear;
+close all;
+
+Fs = input('请输入采样频率:');
+recordt=input('请输入录制时间');
+disp('按任意键开始录音');
+pause;
+recorderObj = audiorecorder(Fs,16,1);
+recordblocking(recorderObj, recordt);
+info = getaudiodata(recorderObj);
+N=length(info);
+disp('录音结束.');
+filename = 'G:\MATLAB2021a\final\wyn\ceshi.wav';
+audiowrite(filename,info,Fs);
+fft_infor = abs(fft(info));
+fft_infor = fft_infor./max(fft_infor);
+T=1/Fs;
+t=(0:N-1)*T;
+df=Fs/N;
+f=(0:N-1)*df;
+
+disp('按任意键播放音乐.');
+pause;
+player0 = audioplayer(info, Fs);
+play(player0);
+pause(recordt);
+disp('结束.')
+
+disp('按任意键显示画面');
+pause;
+figure;
+subplot(1, 2, 1);plot(t,info);title('声音时域信息');xlabel('t');ylabel('audio_infor');
+subplot(1, 2, 2);plot(f,fft_infor,'blue');title('声音的频域信息');xlabel('w/pi');ylabel('fft_audio_infor');
